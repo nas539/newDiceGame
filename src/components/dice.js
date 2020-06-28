bc727dece640753bf4d819c48fed6368a138a648
Nicholas Sharp <naas8905@gmail.com>
2020-06-27 18:09:40 -0700

    Adding the rolling functions

--- src/components/dice.js
+++ src/components/dice.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Chance from 'chance';
+
 import Image1 from '../../static/assets/images/dice1.png';
 import Image2 from '../../static/assets/images/dice2.png';
 import Image3 from '../../static/assets/images/dice3.png';
@@ -7,10 +9,24 @@ import Image5 from '../../static/assets/images/dice5.png';
 import Image6 from '../../static/assets/images/dice6.png';
 import ImageBlank from '../../static/assets/images/blank.png';
 
+const weightedDice = {
+    1: 2,
+    2: 5,
+    3: 5,
+    4: 4,
+    5: 2,
+    6: 6
+}
+let times = 0;
+let diceNumbers = [];
+
 
 export default class Dice extends Component {
+    
     constructor(props) {
         super(props);
+
+        
 
         this.state = {
             dice1: Image1,
@@ -25,13 +41,54 @@ export default class Dice extends Component {
             winningDice4: ImageBlank,
             winningDice5: ImageBlank,
             winningDice6: ImageBlank,
+            howManyDice: 6
         }
+        this.handleClick = this.handleClick.bind(this);
+        this.gettingTheWeightedNumbers = this.gettingTheWeightedNumbers.bind(this);
+        this.settingTheNumberOfTimesToRun = this.settingTheNumberOfTimesToRun.bind(this);
+        this.settingTheImages = this.settingTheImages.bind(this);
     }
+
+    gettingTheWeightedNumbers() {
+        let arrayOfObjects = Object.keys(this.weightedDice);
+        let arrayOfWeights = Object.values(this.weightedDice);
+        return Chance.weighted(arrayOfObjects, arrayOfWeights);
+    }
+
+    settingTheNumberOfTimesToRun() {
+        while (times <= this.howManyDice) {
+           diceNumbers.push(this.gettingTheWeightedNumbers)
+           times += 1;
+        }
+        return diceNumbers;
+    }
+
+    settingTheImages() {
+        let newArray = this.settingTheNumberOfTimesToRun;
+        newArray.forEach(item => {
+            if (item === 1 || item === 5) {
+                
+            }
+        })
+
+    }
+
+
+
+
+
+    handleClick() {
+      
+    }
+
     render() {
         return (
             <div className="game-wrapper">
+                <div className="title">
+                    New Dice Game
+                </div>
                 <div className="rolling-number-wrapper">
-                    <img src={this.state.dice1} height={100} width={100}></img>
+                    <img src={this.state.dice1}></img>
                     <img src={this.state.dice2}></img>
                     <img src={this.state.dice3}></img>
                     <img src={this.state.dice4}></img>
@@ -46,6 +103,8 @@ export default class Dice extends Component {
                     <img src={this.state.winningDice5}></img>
                     <img src={this.state.winningDice6}></img>
                 </div>
+
+                <button type="button" onClick={this.handleClick}>Roll</button>
                 
             </div>
         )
